e33f8140a1103f7e129d5170a8a562124e721156
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2024-10-30 13:31:57 -0500

    Exercise 4 Longest word

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-print (f"Exercise 3 division")
+#Escriba un programa que pida al usuario dos palabras, y que indique cuál de ellas es la más larga y por cuántas letras lo es.
 
-D1 = int(input("insert number: "))
-D2 = int(input("Insert number: "))
+#Palabra 1: edificio
+#Palabra 2: tren
+#La palabra edificio tiene 4 letras mas que tren.
+#Palabra 1: sol
+#Palabra 2: paralelepipedo
+#La palabra paralelepipedo tiene 11 letras mas que sol#
 
-Co = D1 // D2
-Rest = D1 % D2
+print ("Exercise 4 Longest word")
 
-if Rest == 0:
-    print (f"The division is exact.")
+word1 = input("insert a word 1: ")
+word2 = input("insert a word 2: ")
+
+len1 = len(word1)
+len2 = len(word2)
+
+if len1 > len2 :
+    dif1 = len1 - len2
+    print (f"The word {word1} hace {dif1} letters more than {word2}")
+elif len2 > len1: 
+    dif2 = len2 - len1
+    print (f"The word {word2} have {dif2} letters more than {word1}")
 else: 
-    print (f"The division isn't exact.")
-    print (f"quotient: {Co}")
-    print (f"Rest {Rest}")
+    print (f"both words have the same number of letters")
