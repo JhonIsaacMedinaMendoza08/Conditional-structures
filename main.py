60f514c8f8db0240e11e390ce2d3e94bfc5aeada
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2024-10-30 12:53:23 -0500

    Exercise 2 leap year

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-#Escriba un programa que determine si el número entero ingresado por el usuario es par o no.
+#Escriba un programa que indique si un año es bisiesto o no, teniendo en cuenta cuál era el calendario vigente en ese año:
 
-#Ingrese un número: 4
-#Su número es par
-#Ingrese un número: 3
-#Su número es impar
-print ("Exercise 1")
-number = int(input("Insert number, please: "))
+#Ingrese un anno: 1988
+#1988 es bisiesto
+#Ingrese un anno: 2011
+#2011 no es bisiesto
+#Ingrese un anno: 1700
+#1700 no es bisiesto
+#Ingrese un anno: 1500
+#1500 es bisiesto
 
-if number % 2 == 0 :
-    print ("The number is even")
-else: 
-    print("The number is odd")
+print ("Exercise 2")
+year = int(input("Insert year, please: "))
+
+if year % 400 == 0:#or year %400 ==0 :
+    print ("The year is leap year")
+elif year %4 ==0:
+    print ("The year is leap year")
+else:
+    print ("The year isn't leap year")
