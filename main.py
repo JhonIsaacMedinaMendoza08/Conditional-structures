9a6e71b2f22630c25aac39176a308a8d46625e89
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2024-10-30 13:14:06 -0500

    Exercise 3 division

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
-#Escriba un programa que indique si un año es bisiesto o no, teniendo en cuenta cuál era el calendario vigente en ese año:
+print (f"Exercise 3 division")
 
-#Ingrese un anno: 1988
-#1988 es bisiesto
-#Ingrese un anno: 2011
-#2011 no es bisiesto
-#Ingrese un anno: 1700
-#1700 no es bisiesto
-#Ingrese un anno: 1500
-#1500 es bisiesto
+D1 = int(input("insert number: "))
+D2 = int(input("Insert number: "))
 
-print ("Exercise 2")
-year = int(input("Insert year, please: "))
+Co = D1 // D2
+Rest = D1 % D2
 
-if year % 400 == 0:#or year %400 ==0 :
-    print ("The year is leap year")
-elif year %4 ==0:
-    print ("The year is leap year")
-else:
-    print ("The year isn't leap year")
+if Rest == 0:
+    print (f"The division is exact.")
+else: 
+    print (f"The division isn't exact.")
+    print (f"quotient: {Co}")
+    print (f"Rest {Rest}")
