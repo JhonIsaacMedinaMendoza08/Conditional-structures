ed757bcd7fb3d1ae55caacba8e3ecdd59e236bd9
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2024-10-31 11:05:03 -0500

    Exercise 5.1

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
-#Escriba un programa que pida al usuario dos palabras, y que indique cuál de ellas es la más larga y por cuántas letras lo es.
+#Programa para ordenar dos números:
 
-#Palabra 1: edificio
-#Palabra 2: tren
-#La palabra edificio tiene 4 letras mas que tren.
-#Palabra 1: sol
-#Palabra 2: paralelepipedo
-#La palabra paralelepipedo tiene 11 letras mas que sol#
+number1 = int(input("insert number: "))
+number2 = int(input("insert number: "))
 
-print ("Exercise 4 Longest word")
-
-word1 = input("insert a word 1: ")
-word2 = input("insert a word 2: ")
-
-len1 = len(word1)
-len2 = len(word2)
-
-if len1 > len2 :
-    dif1 = len1 - len2
-    print (f"The word {word1} hace {dif1} letters more than {word2}")
-elif len2 > len1: 
-    dif2 = len2 - len1
-    print (f"The word {word2} have {dif2} letters more than {word1}")
-else: 
-    print (f"both words have the same number of letters")
+if number1 > number2:
+    print(number2, number1)
+else:
+    print(number1, number2)
