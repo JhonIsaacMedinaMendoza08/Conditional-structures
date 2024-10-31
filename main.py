2be3f38c81bfb7143a00521ba97343b6e9bc827f
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2024-10-31 11:09:42 -0500

    Exercise 5.3

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-#Programa para ordenar tres números:
+#Programa para ordenar cuatro números:
 
 print("Exercise 5 sort numbers")
 
 number1 = int(input("insert number: "))
 number2 = int(input("insert number: "))
 number3 = int(input("insert number: "))
+number4 = int(input("insert number: "))
 
-numbers = [number1 ,number2, number3]
+numbers = [number1 ,number2, number3, number4]
 numbers.sort()
 
 print(numbers)
